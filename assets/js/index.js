d3d5b3b554c2e59892e7743cd750931556f85a0f
haozi23333 <haozi23333@gmail.com>
2017-03-22 21:05:31 +0800

    懶得寫

--- assets/js/index.js
+++ assets/js/index.js
@@ -36,19 +36,19 @@
         });
 
         // highlight config
-        hljs.initHighlightingOnLoad();
+        // hljs.initHighlightingOnLoad();
 
         // numbering for pre>code blocks
-        $(function() {
-            $('pre code').each(function() {
-                var lines = $(this).text().split('\n').length - 1;
-                var $numbering = $('<ul/>').addClass('pre-numbering');
-                $(this).addClass('has-numbering').parent().append($numbering);
-                for (var i = 1; i <= lines; i++) {
-                    $numbering.append($('<li/>').text(i));
-                }
-            });
-        });
+        // $(function() {
+        //     $('pre code').each(function() {
+        //         var lines = $(this).text().split('\n').length - 1;
+        //         var $numbering = $('<ul/>').addClass('pre-numbering');
+        //         $(this).addClass('has-numbering').parent().append($numbering);
+        //         for (var i = 1; i <= lines; i++) {
+        //             $numbering.append($('<li/>').text(i));
+        //         }
+        //     });
+        // });
 
         var toc = $('.toc');
         // toc config
@@ -136,7 +136,6 @@
         },
 
         allOptions = $.extend(defaults, options);
-
         allOptions.elem.click(function(event) {
             event.preventDefault();
             var $this = $(this),
@@ -162,6 +161,8 @@
             }
         });
     };
+
+    aceInit()
 })(jQuery);
 
 function scrollToTop(name, speed) {
@@ -177,4 +178,32 @@ function scrollToTop(name, speed) {
             }, speed)
         }
     }
+}
+
+// 背景彩带
+// jQuery('body').append('<canvas id="caidai" style="z-index: 9999999999999999999999;position: fixed;top: 1px;pointer-events: none;"></canvas>');
+// var c=document.getElementsByTagName("canvas")[0],x=c.getContext("2d"),pr=window.devicePixelRatio||1,w=window.innerWidth,h=window.innerHeight,f=90,q,m=Math,r=0,u=m.PI*2,v=m.cos,z=m.random;c.width=w*pr;c.height=h*pr;x.scale(pr,pr);x.globalAlpha=0.6;function i(){x.clearRect(0,0,w,h);q=[{x:0,y:h*0.7+f},{x:0,y:h*0.7-f}];while(q[1].x<w+f){d(q[0],q[1])}}function d(e,b){x.beginPath();x.moveTo(e.x,e.y);x.lineTo(b.x,b.y);var a=b.x+(z()*2-0.25)*f,g=y(b.y);x.lineTo(a,g);x.closePath();r-=u/-50;x.fillStyle="#"+(v(r)*127+128<<16|v(r+u/3)*127+128<<8|v(r+u/3*2)*127+128).toString(16);x.fill();q[0]=q[1];q[1]={x:a,y:g}}function y(b){var a=b+(z()*2-1.1)*f;return(a>h||a<0)?y(b):a}document.onclick=i;document.ontouchstart=i;i();/*1488120172*/
+
+
+function aceInit() {
+    if(document.getElementsByTagName('pre').length == 0){
+        console.log('Ace.js 未加载 -> 没有找到代码段')
+        return
+    }
+    $.getScript('../assets/ace-min-1.2.6/ace.js').done(function () {
+        console.log('Ace.js 加载完成')
+        var highlight = ace.require("ace/ext/static_highlight")
+        var dom = ace.require("ace/lib/dom")
+        $('pre').map(function (index, el) {
+            highlight(el, {
+                mode: el.getAttribute("ace-mode") || el.className.split('-').pop(),
+                theme: el.getAttribute("ace-theme") || 'chrome',
+                startLineNumber: 1,
+                showGutter: el.getAttribute("ace-gutter") || true,
+                trim: true
+            })
+        })
+    }).fail(function () {
+        console.log('Ace.js 未加载 -> ace.js 文件不存在')
+    })
 }
